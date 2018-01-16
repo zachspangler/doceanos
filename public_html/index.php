b3a2d195d3132c5e5bce33a8a7b588b364791d91
Zach Spangler <zaspangler@gmail.com>
2018-01-16 09:48:38 -0700

    added content to htm page

---
+++ public_html/index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required Meta Tags -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap Link -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+				integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- Font Awesome Link -->
+		<script src="https://use.fontawesome.com/46c6e23a21.js"></script>
+
+		<!-- Doce Años CSS -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+				  integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+				  integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- Doce Años Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+
+
+		<!--Google Fonts -->
+
+
+		<!-- Doce Años Favicon -->
+		<link rel="shortcut icon" href="https://dl.dropboxusercontent.com/s/tnl4e63cixdw13r/skapa_favicon.png?dl=0" type="image/x-icon">
+
+		<title>Doce Años</title>
+	</head>
+	<body>
+		<!-- Navbar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Doce Años Open Space</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Options</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Video</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Gallery</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<!-- Carousel -->
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="https://www.theknot.com/marketplace/images/vendor_cards/wph.jpg" alt="First slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="https://assets.marthastewartweddings.com/styles/wmax-1500/d49/aislinn-eric-wedding-malibu-bride-groom-102896024/aislinn-eric-wedding-malibu-bride-groom-102896024_horiz.jpg?itok=2X6AtG4N" alt="Second slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="http://a058c04a4401567c8d82-13eef12c52811a9a79fad905d437e8eb.r15.cf1.rackcdn.com/lps/assets/u/Richmond-Wedding.jpg" alt="Third slide">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+		<!-- About Us -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<h3>Doce Años Open Space</h3>
+					<p>Doce Años Open Space was started to showcase New Mexico's natural beauty</p>
+					<p>Located 10 minutes from Bernallio we can host any event</p>
+				</div>
+			</div>
+		</div>
+		<!-- Video -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<iframe width="420" height="315"
+							  src="https://www.youtube.com/embed/tgbNymZ7vqY?controls=0">
+					</iframe>
+				</div>
+			</div>
+		</div>
+		<!-- Rental Types -->
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-5">
+					<img src="https://moncheribridals.com/wp-content/themes/moncheri2017/images/118/mobile/wedding.jpg">
+				</div>
+				<div class="col-md-7">
+					<h4>Events</h4>
+					<p>This is an event description</p>
+					<a href="#">Check out the gallery</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-7">
+					<h4>Elopements</h4>
+					<p>This is an elopement description</p>
+					<a href="#">Check out the gallery</a>
+				</div>
+				<div class="col-md-5">
+					<img src="https://moncheribridals.com/wp-content/themes/moncheri2017/images/118/mobile/wedding.jpg">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-5">
+					<h4>Other</h4>
+					<p>This is the other description</p>
+					<a href="#">Check out the gallery</a>
+				</div>
+				<div class="col-md-7">
+					One of three columns
+				</div>
+			</div>
+		</div>
+		<!-- Contact Us -->
+		<section id="contact-us">
+			<h2 class="h1" id="contact-us-text">Interested in working together?</h2>
+			<p class="contact-us-subtext">We are always looking for new partners and projects</p>
+			<p class="contact-us-subtext">Shoot us an email or fill out the contact form below to get the conversation
+				started</p>
+			<div id="accordion" role="tablist">
+				<div class="card">
+					<div class="card-header" role="tab" id="headingOne">
+						<h5 class="mb-0">
+							<a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								EMAIL US
+							</a>
+						</h5>
+					</div>
+					<div class="container-fluid contact-form">
+						<div class="row">
+							<div class="col-md-4"></div>
+							<div class="col-md-4">
+								<div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne">
+									<div class="input-group">
+										<!-- START CONTACT FORM -->
+										<form class="align-content-center" id="skapa-development-form" action="php/mailer.php" method="post" novalidate>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-addon">
+														<i class="fa fa-user" aria-hidden="true"></i>
+													</div>
+													<input type="text" id="name" name="name" class="form-control" placeholder="Name">
+												</div>
+											</div>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-addon">
+														<i class="fa fa-envelope" aria-hidden="true"></i>
+													</div>
+													<input type="email" class="form-control" id="email" name="email"
+															 placeholder="Email">
+												</div>
+											</div>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-addon">
+														<i class="fa fa-phone" aria-hidden="true"></i>
+													</div>
+													<input type="tel" class="form-control" id="phone" name="phone"
+															 placeholder="Phone">
+												</div>
+											</div>
+											<div class="form-group">
+												<div class="input-group">
+													<div class="input-group-addon">
+														<i class="fa fa-comment" aria-hidden="true"></i>
+													</div>
+													<textarea id="message" name="message" class="form-control"
+																 placeholder="Message"></textarea>
+												</div>
+											</div>
+											<!-- reCAPTCHA -->
+											<div class="g-recaptcha" data-sitekey="6Le6CjoUAAAAAFwrgt0yTT0qQWklid4Z1jnhhHY_"></div>
+											<button class="submit-button" type="submit"><i class="fa fa-paper-plane"></i> Send
+											</button>
+											<button class="reset-button" type="reset"><i class="fa fa-ban"></i> Reset</button>
+										</form>
+										<!-- END CONTACT FORM -->
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4"></div>
+						</div>
+						<div class="row output-row">
+							<div class="col-md-12">
+								<div id="output-area"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--empty area for form error/success output-->
+		</section>
+	</body>
